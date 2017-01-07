75b046cfdd29b555b8b873c6ffb3fc762b74098b
Rob Hitt <robhitt@gmail.com>
2017-01-07 00:17:44 -0500

    done

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,4 +11,29 @@ class AttractionsController < ApplicationController
     #@attraction = Attraction.find()
   end
 
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attractions_params)
+    @attraction.save
+    redirect_to attraction_path(@attraction)
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+  end
+
+  private
+
+
+  def attractions_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating,
+    :happiness_rating, :min_height)
+  end
+
 end
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -7,8 +7,8 @@ class RidesController < ApplicationController
     @attraction = Attraction.find(params[:attraction_id])
     @ride = Ride.create(attraction_id: @attraction.id, user_id: @user.id)
 
-    @ride.take_ride
-    flash[:notice] = "Thanks for riding the #{@attraction.name}!"
+    flash[:notice] = @ride.take_ride
+
     redirect_to user_path(@user)
   end
 end
